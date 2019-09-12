2d22645ce5dae89a77de02f13756a415545bb072
Michal Muszynski <m.muszy@gmail.com>
2019-09-12 21:05:18 +0200

    Colors definitions

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,19 +1,17 @@
-export const colors = {
-  main: {
-    white: "#fff",
-    black: "#00393f",
-    green: "#007261",
-    lightBrown: "#fee5c2"
-  },
-  tag: {
-    red: "#c33200",
-    darkBlue: "#007da6",
-    woodBrown: "#da9e65"
-  },
-  highlights: {
-    lightGreen: "#00aa60",
-    indianRed: "#ff6c64",
-    teal: "#00abbe",
-    darkYellow: "#ffdc65"
-  }
+interface Colors {
+  [key: string]: string
+}
+
+export const colors: Colors = {
+  white: "#fff",
+  black: "#00393f",
+  green: "#007261",
+  lightBrown: "#feeaca",
+  red: "#c33200",
+  darkBlue: "#007da6",
+  woodBrown: "#da9e65",
+  lightGreen: "#00aa60",
+  indianRed: "#ff6c64",
+  teal: "#00abbe",
+  darkYellow: "#ffdc65"
 }
