24472ddb3dc11656462494e0c6f415220022a78b
Michal Muszynski <m.muszy@gmail.com>
2019-12-03 19:30:53 +0100

    Change component to pure funcion

--- src/components/Map/map.tsx
+++ src/components/Map/map.tsx
@@ -2,24 +2,22 @@ import * as React from "react"
 import { Map, TileLayer, Marker, Popup } from "react-leaflet"
 import { mapSettings } from "./map-utils"
 
-class MapComponent extends React.Component {
-  render() {
-    return (
-      <Map
-        center={mapSettings.coordinates}
-        zoom={mapSettings.zoom}
-        style={{ height: "100%" }}
-      >
-        <TileLayer
-          url={mapSettings.mainTile}
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={mapSettings.coordinates}>
-          <Popup>The Best Karl Was born here!</Popup>
-        </Marker>
-      </Map>
-    )
-  }
+const MapComponent = () => {
+  return (
+    <Map
+      center={mapSettings.coordinates}
+      zoom={mapSettings.zoom}
+      style={{ height: "100%" }}
+    >
+      <TileLayer
+        url={mapSettings.mainTile}
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={mapSettings.coordinates}>
+        <Popup>The Best Karl Was born here!</Popup>
+      </Marker>
+    </Map>
+  )
 }
 
 export default MapComponent
