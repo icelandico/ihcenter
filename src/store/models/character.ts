139dec9343ecfbbb8346ecd4fff503c0a5ec8cc7
Michal Muszynski <m.muszy@gmail.com>
2019-12-05 21:24:13 +0100

    Fetch character data

--- src/store/models/character.ts
+++ src/store/models/character.ts
@@ -1,17 +1,33 @@
-import { types } from "mobx-state-tree"
+import { types, Instance, cast, flow } from "mobx-state-tree"
+import { apiUrls } from "../api/api"
 
-const Character = types.model("Character", {
+export type CharacterModel = Instance<typeof Character>
+export type CharacterListModel = Instance<typeof CharacterList>
+
+export const Character = types.model("Character", {
   id: types.number,
-  fullName: types.string,
-  birth: types.string,
-  death: types.string,
-  coords: types.string,
-  imageUrl: types.string,
-  wikipediaLink: types.string,
-  description: types.string,
+  fullName: types.optional(types.string, "")
+  birth: types.optional(types.string, ""),
+  death: types.optional(types.string, ""),
+  coords: types.optional(types.string, ""),
+  imageUrl: types.optional(types.string, ""),
+  wikipediaLink: types.optional(types.string, ""),
+  description: types.optional(types.string, ""),
   nationality: types.model()
 })
 
-export const CharacterList = types.optional(types.array(Character), [])
-
-export default Character
+export const CharacterList = types
+  .model("Characters", {
+    characters: types.optional(types.array(Character), []),
+    names: "Example Name"
+  })
+  .actions(self => ({
+    // setCharacters(characterList: []) {
+    //   self.characters.concat(cast(characterList))
+    // },
+    getCharacterList: flow(function*() {
+      const response = yield fetch("http://localhost:1337/characters")
+      const characters = yield response.json()
+      self.characters = characters
+    })
+  }))
