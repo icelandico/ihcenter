3c919c6977c7255c7c070152fd27c85717ee1acd
Michal Muszynski <m.muszy@gmail.com>
2019-12-08 13:34:51 +0100

    Added api endpoints

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -3,5 +3,12 @@ interface Urls {
 }
 
 export const apiUrls: Urls = {
-  characters: "http://localhost:1337/characters"
+  characters: "http://localhost:1337/characters",
+  events: "http://localhost:1337/events",
+  ideas: "http://localhost:1337/ideas",
+  literature: "http://localhost:1337/literature",
+  professions: "http://localhost:1337/professions",
+  mainideas: "http://localhost:1337/mainideas",
+  organisations: "http://localhost:1337/organisations",
+  nationalities: "http://localhost:1337/nationalities"
 }
