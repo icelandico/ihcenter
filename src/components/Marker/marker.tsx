8957bfa5b614989343e2197023fa13f0a433a04c
Michal Muszynski <m.muszy@gmail.com>
2019-12-17 20:47:56 +0100

    Display conditional icon

--- src/components/Marker/marker.tsx
+++ src/components/Marker/marker.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import { Marker, Popup } from "react-leaflet"
 import { observer, inject } from "mobx-react"
-import styled from "styled-components"
 import L from "leaflet"
 import ReactDOMServer from "react-dom/server"
 import { rootStore } from "../../store/RootStore"
-import { icon, iconClicked } from "../MapIcons/map-icon"
+import { icon } from "../MapIcons/map-icon"
+import DefaultIcon from "../../static/icons/person.svg"
 
 interface Props {
   store?: typeof rootStore
@@ -19,38 +19,28 @@ interface State {
   clicked: boolean
 }
 
-const divMarker = styled.div`
-  width: 30px !important;
-  height: 30px !important;
-  border-radius: 50%;
-  background-image: url("./static/icons/person.svg");
-  background-size: contain;
-  background-repeat: no-repeat;
-`
-
-const customIcon = L.divIcon({
-  html: ReactDOMServer.renderToString(
-    <div className="custom-marker">
-      <div
-        className="marker-icon"
-        style={{
-          backgroundImage:
-            "url('https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Karl_Marx.jpg/205px-Karl_Marx.jpg')"
-        }}
-      />
-    </div>
-  ),
-  iconAnchor: [15, 30]
-})
-
 class MapMarker extends React.Component<Props, State> {
   state = {
     clicked: false
   }
 
+  customIcon = L.divIcon({
+    html: ReactDOMServer.renderToString(
+      <div className="custom-marker">
+        <div
+          className="marker-icon"
+          style={{
+            backgroundImage: `url(${this.props.image || DefaultIcon})`
+          }}
+        />
+      </div>
+    ),
+    iconAnchor: [15, 30]
+  })
+
   chooseIcon = () => {
     const { clicked } = this.state
-    return clicked ? customIcon : icon
+    return clicked ? this.customIcon : icon
   }
 
   switchIcon = () => {
