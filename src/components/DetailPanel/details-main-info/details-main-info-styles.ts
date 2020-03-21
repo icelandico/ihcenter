aaca75a7ee1bd19edea228b8d26740d085c824e6
Michal Muszynski <m.muszy@gmail.com>
2020-03-21 22:19:58 +0100

    Change main info style

--- src/components/DetailPanel/details-main-info/details-main-info-styles.ts
+++ src/components/DetailPanel/details-main-info/details-main-info-styles.ts
@@ -4,29 +4,30 @@ import wikiIcon from "../../../static/icons/wiki.svg"
 
 export const DetailsTopContainer = styled.div`
   position: relative;
-  margin-top: -40px;
+  margin-top: -4rem;
 `
 
 export const ElementTitle = styled.h1`
   text-align: center;
-  font-size: 24px;
+  font-size: 2.4rem;
   border-bottom: 1px solid ${colors.lightBrown};
-  margin-bottom: 10px;
-  padding-bottom: 10px;
+  padding-bottom: 1rem;
+  margin: 0 auto 1rem;
+  width: 75%;
 `
 
 export const ElementDate = styled.p`
   text-align: center;
-  font-size: 16px;
+  font-size: 1.6rem;
   color: ${colors.lightBrown};
 `
 
 export const MainImage = styled.div`
   position: relative;
   top: -50px;
-  width: 160px;
-  border: 6px solid ${colors.darkGreen};
-  height: 160px;
+  width: 16rem;
+  border: 0.5rem solid ${colors.darkGreen};
+  height: 16rem;
   border-radius: 50%;
   background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6x-rKSUYJJ6aa673JE2ZsjVcvhoIL6v3tAI_1X8Br56U4VrrL&s");
   background-size: cover;
@@ -34,38 +35,30 @@ export const MainImage = styled.div`
 `
 
 export const DetailsContainer = styled.div`
-  max-height: 60%;
+  height: 62%;
   overflow-y: scroll;
   position: absolute;
   bottom: 0;
-  scrollbar-width: thin;
-  scrollbar-color: teal red;
   margin-bottom: 1rem;
+  padding: 0 1rem;
+
+  &::-webkit-scrollbar {
+    width: 0.5rem;
+    height: 1.5rem;
+  }
 
   &::-webkit-scrollbar-track {
     background: gray;
   }
   &::-webkit-scrollbar-thumb {
     background-color: white;
-    border-radius: 6px;
-    border: 3px solid red;
-  }
-
-  &::after {
-    /* content: "";
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100%;
-    /* box-shadow: inset 0px -65px 55px 0px rgba(0, 57, 63, 0.9); 
-    background: linear-gradient(transparent 550px, white); */
+    border-radius: 0.5rem;
+    border: 0.3rem solid ${colors.darkYellow};
   }
 `
 
 export const DetailsText = styled.p`
-  font-size: 16px;
+  font-size: 1.5rem;
   color: ${colors.lightBrown};
   padding-bottom: 2rem;
 `
