7238f0215f58756b443911b221e5d5421705b315
Michal Muszynski <m.muszy@gmail.com>
2020-07-03 23:28:04 +0200

    Divide component to menu and content parts

--- src/components/Timeline/index-styles.ts
+++ src/components/Timeline/index-styles.ts
@@ -3,9 +3,14 @@ import { colors } from "../../styles/colors"
 import { fonts } from "../../styles/font"
 
 export const TimelineContainer = styled.div`
-  min-height: 6rem;
+  display: flex;
+  min-height: 9rem;
   outline: 1px solid rgb(196, 177, 8);
   width: 100%;
   z-index: 5;
-  padding: 1rem;
+`
+
+export const TimelineMenu = styled.div`
+  width: 5%;
+  background-color: navy;
 `
--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react"
 import { inject, observer } from "mobx-react"
-import { TimelineContainer } from "./index-styles";
+import { TimelineContainer, TimelineMenu } from "./index-styles"
 
 interface Props {}
 
 const Timeline: React.FC<Props> = props => {
   return (
     <TimelineContainer>
-      <p>Hello Timeline</p>
+      <TimelineMenu />
+      <div className="timeline-main" />
     </TimelineContainer>
   )
 }
