b393da58660d083a67dc21708e05993518cb6c49
Michal Muszynski <m.muszy@gmail.com>
2020-07-04 09:56:57 +0200

    Created container for date picker

--- src/components/Timeline/timeline-date-picker/timeline-date-picker-styles.ts
+++ src/components/Timeline/timeline-date-picker/timeline-date-picker-styles.ts
@@ -3,7 +3,20 @@ import { colors } from "../../../styles/colors"
 import { fonts } from "../../../styles/font"
 
 export const TimelineDateContainer = styled.div`
-  height: 3rem;
-  background-color: #ccc;
-  opacity: 0.1;
+  height: 4rem;
+  outline: 1px solid chocolate;
+  margin: 0 auto;
+  width: 14rem;
+  color: chocolate;
+  text-align: center;
+  
+  & p {
+    font-size: 36px;
+  }
 `
+
+// export const TimelineDateContainer = styled.div`
+//   height: 3rem;
+//   background-color: #ccc;
+//   opacity: 0.1;
+// `
