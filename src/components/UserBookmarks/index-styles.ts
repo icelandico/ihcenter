34e3b84750fc6b9208b875149fa3e9ee01737777
Michal Muszynski <m.muszy@gmail.com>
2020-11-20 23:50:59 +0100

    Create component for user bookmarks

--- src/components/UserBookmarks/index-styles.ts
+++ src/components/UserBookmarks/index-styles.ts
@@ -2,6 +2,51 @@ import styled from "styled-components"
 import { colors } from "../../styles/colors"
 
 export const UserBookmarksContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
   height: 6rem;
-  outline: 0.1rem solid rgb(196, 177, 8);
+  margin-left: 3rem;
+  border-bottom: 0.1rem solid ${colors.lightBrown};
+  border-left: 0.1rem solid ${colors.lightBrown};
+`
+
+export const UserBookmarksText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+`
+
+export const OptionsText = styled.p`
+  font-size: 1.4rem;
+  padding: 0.5rem 1rem;
+  color: ${colors.lightBrown};
+
+  &:first-child {
+    border-bottom: 0.2rem solid ${colors.green};
+  }
+`
+
+export const UserRecentlyViewedItems = styled.div`
+  display: flex;
+  flex-basis: 85%;
+  justify-content: flex-end;
+`
+
+export const UserBookmarksItems = styled.div`
+  position: absolute;
+  display: flex;
+  width: 65%;
+  border: 1px solid darkgoldenrod;
+  height: 3rem;
+  top: 100%;
+  right: 0;
+`
+
+export const RecentItem = styled.div`
+  height: 2.5rem;
+  width: 2.5rem;
+  background-color: peru;
+  border-radius: 50%;
 `
--- src/components/UserBookmarks/index.tsx
+++ src/components/UserBookmarks/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { observer, inject } from "mobx-react"
 import { rootStore } from "../../store/RootStore"
-import { UserBookmarksContainer } from "./index-styles"
+import { UserBookmarksContainer, RecentItem, UserRecentlyViewedItems, UserBookmarksItems, UserBookmarksText, OptionsText } from "./index-styles"
 
 export interface IProps {
   store?: typeof rootStore
@@ -11,7 +11,18 @@ const UserBookmarks: React.FC<IProps> = props => {
   const { store } = props
 
   return (
-    <UserBookmarksContainer />
+    <UserBookmarksContainer>
+      <UserBookmarksText>
+        <OptionsText>RECENT:</OptionsText>
+        <OptionsText>BOOKMARKS:</OptionsText>
+      </UserBookmarksText>
+      <UserRecentlyViewedItems>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(el => {
+          return <RecentItem />
+        })}
+      </UserRecentlyViewedItems>
+      <UserBookmarksItems />
+    </UserBookmarksContainer>
   )
 }
 
