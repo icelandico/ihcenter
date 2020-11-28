ff25e934c33190c6ee1287dd6788ae19e9b78fcd
Michal Muszynski <m.muszy@gmail.com>
2020-11-28 23:03:36 +0100

    Change styles for bookmarks placeholder

--- src/components/UserBookmarks/user-bookmarks-items/user-bookmarks-items-styles.ts
+++ src/components/UserBookmarks/user-bookmarks-items/user-bookmarks-items-styles.ts
@@ -4,4 +4,6 @@ import { colors } from "../../../styles/colors"
 export const UserBookmarksPlaceholder = styled.p`
   font-size: 1.8rem;
   color: ${colors.lightBrown};
+  background-color: ${colors.darkGreen};
+  padding: 0 0.5rem;
 `
