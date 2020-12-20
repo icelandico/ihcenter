8d087adff13a6f8433e08eaa85565ab12ac5df71
Michal Muszynski <m.muszy@gmail.com>
2020-12-20 20:44:40 +0100

    Position FilterTab

--- src/components/Filters/filterTab/filter-tab-styles.ts
+++ src/components/Filters/filterTab/filter-tab-styles.ts
@@ -5,12 +5,13 @@ export const FilterTabContainer = styled.div<{
   isActive: boolean
 }>`
   position: absolute;
-  left: 130%;
+  left: 4rem;
   top: 0;
-  background-color: ${colors.green};
+  background-color: ${colors.darkGreen};
   padding: 1rem;
   z-index: 2000;
   height: 400px;
   width: 150px;
   display: ${props => (props.isActive ? "unset" : "none")};
+  font-size: 1.6rem;
 `
