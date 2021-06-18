3780297da1b8f492557612e711ef2716460a1222
Michal Muszynski <m.muszy@gmail.com>
2021-06-18 23:58:30 +0200

    Remove redundant imports

--- src/components/Filters/filter-checkbox/filter-checkbox.tsx
+++ src/components/Filters/filter-checkbox/filter-checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect } from "react"
+import React, { FunctionComponent } from "react"
 import { inject, observer } from "mobx-react"
 import { getSnapshot } from "mobx-state-tree"
 import {
